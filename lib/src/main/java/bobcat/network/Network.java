c9013421c96224cd82dbbc4e7f22c18bcef98227
Ivan R. Judson <ivan.judson+github@webfilings.com>
2012-11-20 06:37:54 -0700

    Moved interference to it's own method for factoring and reuse.

--- lib/src/main/java/bobcat/network/Network.java
+++ lib/src/main/java/bobcat/network/Network.java
@@ -1,5 +1,6 @@
 package bobcat.network;
 
+import edu.uci.ics.jung.graph.util.Pair;
 import edu.uci.ics.jung.graph.Graph;
 import edu.uci.ics.jung.graph.UndirectedSparseGraph;
 
@@ -69,6 +70,64 @@ public class Network<V, E>
         return((Vertex)v.get(this.random.nextInt(v.size())));
     }
 
+    public void printInterferenceTable() {
+        for (int i = 0; i < this.interferes.length; i++) {
+            for (int j = 0; j < this.interferes[i].length; j++) {
+                for (int k = 0; k < this.interferes[i][j].length; k++) {
+                    System.out.println(i+","+j+","+k+"="+this.interferes[i][j][k]);
+                }
+            }
+        }
+    }
+
+    public void computeInterference() {
+        //Build interference table
+        for (E e1a : this.getEdges()) {
+            Edge e1 = (Edge)e1a;
+            for (E e2a : this.getEdges()) {
+                Edge e2 = (Edge)e2a;
+                if (e1 != e2) {
+                    Pair<V> v1a = this.getEndpoints(e1a);
+                    Pair<V> v2a = this.getEndpoints(e2a);
+                    Vertex v1f = (Vertex) v1a.getFirst();
+                    Vertex v1s = (Vertex) v1a.getSecond();
+                    Vertex v2f = (Vertex) v2a.getFirst();
+                    Vertex v2s = (Vertex) v2a.getSecond();
+                    // System.out.println("Edges: (" + e1.id + " : " + v1f.id + "," + v1s.id+") (" + e2.id + " : " + v2f.id + "," + v2s.id+")");
+                    double dist = v1f.distanceTo(v2f);
+                    double d2 = v1f.distanceTo(v2s);
+                    double d3 = v1s.distanceTo(v2f);
+                    double d4 = v1s.distanceTo(v2s);
+                    dist = (dist < d2) ? dist : d2;
+                    dist = (dist < d3) ? dist : d3;
+                    dist = (dist < d4) ? dist : d4;
+                    for (int i = 0; i < this.numChannels * 3; i++) {
+                        double range = 0.0d;
+                        if (i > 0 && i < this.numChannels) {
+                            range = 30.8;
+                        } else if (i >= 0 + this.numChannels &&
+                                i < 2 * this.numChannels) {
+                            range = 9.0;
+                        } else if (i >= 0 + 2 * this.numChannels &&
+                                i < 3 * this.numChannels) {
+                            range = 3.6;
+                        }
+                        // System.out.println(" - "+i+" -> D: " + dist + " R: "+ range);
+                        // System.out.println(v1f+" "+v1s+" "+v2f+" "+v2s);
+                        if (e1.channels[i] > 0.0 && e2.channels[i] > 0.0) {
+                            if (dist < range || (v1f == v2f || v1f == v2s || v1s == v2s || v1s == v2f)) {
+                                this.interferes[e1.id][e2.id][i] = true;
+                                this.interferes[e2.id][e1.id][i] = true;                                
+                            } else {
+                                this.interferes[e1.id][e2.id][i] = false;
+                                this.interferes[e2.id][e1.id][i] = false;                                                            
+                            }
+                        }
+                    }
+                }
+            }
+        }        
+    }
     public void calculateBeamSets() {
         int numRelays = relayList.length;
         int numSubs = subList.length;
