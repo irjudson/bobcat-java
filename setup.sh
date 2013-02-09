31b68965f1440ce401a7592ecf09e2ce7175f657
Ivan R. Judson <ivan.judson+github@webfilings.com>
2013-02-09 14:38:53 -0700

    renamed project

--- setup.sh
+++ setup.sh
@@ -18,9 +18,9 @@ export JAVA_OPTIONS="-Djava.library.path=${JLPATH} -Xmx4096m"
 alias java='java $JAVA_OPTIONS'
 
 # run the cognitive radio topology control simulation
-alias crtc='cd $BOBCAT_HOME; java -jar whitespace_short_circuit/target/whitespace_short_circuit-1.0-jar-with-dependencies.jar'
+alias crtc='cd $BOBCAT_HOME; java -jar cognitive_radio_topology_control/target/cognitive_radio_topology_control-1.0-jar-with-dependencies.jar'
 # Run the cognitive radio topology control simulation in debug mode
-alias crtcd='cd $BOBCAT_HOME; java -jar whitespace_short_circuit/target/whitespace_short_circuit-1.0-jar-with-dependencies.jar -d -n 8 -v'
+alias crtcd='cd $BOBCAT_HOME; java -jar cognitive_radio_topology_control/target/cognitive_radio_topology_control-1.0-jar-with-dependencies.jar -d -n 8 -v'
 # Run the routing and channel selection simulation
 alias rcs='cd $BOBCAT_HOME; java -jar routing_channel_selection/target/routing_channel_selection-1.0-jar-with-dependencies.jar'
 # Run the beam scheduling simulation
