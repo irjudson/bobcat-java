2b97e908f8062c722517f83d689719d17aaf7c26
Ivan R. Judson <irjudson@gmail.com>
2013-03-11 17:49:06 -0600

    Updated to build on cygwin on laptop

--- setup.sh
+++ setup.sh
@@ -1,26 +1,30 @@
 #!/usr/bin/env bash
 
-export BOBCAT_HOME="/Users/ivan.judson/Personal/Phd/bobcat"
+# CPLEX Configuration
+CPLEXPATH="/opt/IBM/ILOG/CPLEX_Studio125"
+export CPLEXARCH="x86_win32"
 
-CPLEXPATH="/opt/ibm/ILOG/CPLEX_Studio_Academic123"
-CPLEXPATH="/opt/cplex"
-CPLEXARCH="x86_sles10_4.1"
-CPLEXARCH="x86-64_sles10_4.1"
-export CPLEXARCH="x86-64_darwin9_gcc4.0"
 export JLPATH="${CPLEXPATH}/cplex/bin/${CPLEXARCH}:${CPLEXPATH}/lib:${CPLEXPATH}/lib/${CPLEXARCH}"
 
-JAVA_OPTIONS="-Djava.library.path=${JLPATH} -Xmx4096m"
-export JAVA_OPTIONS="-Djava.library.path=${JLPATH} -Xmx4096m"
+# Setting Java Options
+JAVA_OPTIONS="-Djava.library.path=${JLPATH}"
+export JAVA_OPTIONS
 
-alias java='java $JAVA_OPTIONS'
+alias java='java -d32 -Djava.library.path=${JLPATH}'
+
+CPLEXPATH="/opt/IBM/ILOG/CPLEX_Studio125"
+export CPLEXARCH="x86_win32"
+export JLPATH="${CPLEXPATH}/cplex/bin/${CPLEXARCH}/:${CPLEXPATH}/cplex/lib/:${CPLEXPATH}/lib/${CPLEXARCH}/"
+
+export JAVA="java -d32 -Djava.library.path=${JLPATH}"
 
 # run the cognitive radio topology control simulation
-alias crtc='cd $BOBCAT_HOME; java -jar cognitive_radio_topology_control/target/cognitive_radio_topology_control-1.0-jar-with-dependencies.jar'
+alias crtc='$JAVA -jar cognitive_radio_topology_control/target/cognitive_radio_topology_control-1.0-jar-with-dependencies.jar'
 # Run the cognitive radio topology control simulation in debug mode
-alias crtcd='cd $BOBCAT_HOME; java -jar cognitive_radio_topology_control/target/cognitive_radio_topology_control-1.0-jar-with-dependencies.jar -d -n 8 -v'
+alias crtcd='$JAVA -jar cognitive_radio_topology_control/target/cognitive_radio_topology_control-1.0-jar-with-dependencies.jar -d -n 8 -v'
 # Run the routing and channel selection simulation
-alias rcs='cd $BOBCAT_HOME; java -jar routing_channel_selection/target/routing_channel_selection-1.0-jar-with-dependencies.jar'
+alias rcs='$JAVA -jar routing_channel_selection/target/routing_channel_selection-1.0-jar-with-dependencies.jar'
 # Run the beam scheduling simulation
-alias bs='cd $BOBCAT_HOME; java -jar beam_scheduling/target/beam_scheduling-1.0-jar-with-dependencies.jar'
+alias bs='$JAVA -jar beam_scheduling/target/beam_scheduling-1.0-jar-with-dependencies.jar'
 # Run the directional antenna simulation
-alias da='cd $BOBCAT_HOME; java -jar directional_antennas/target/directional_antennas-1.0-jar-with-dependencies.jar'
+alias da='$JAVA -jar directional_antennas/target/directional_antennas-1.0-jar-with-dependencies.jar'
